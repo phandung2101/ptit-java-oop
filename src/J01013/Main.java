7fc4ba41ebe7cf0aedbbd42a90dc06abdd73d5a1
phandung2101 <dungpv.dev@gmail.com>
2024-09-07 16:40:51 +0700

    1 ac

--- src/J01013/Main.java
+++ src/J01013/Main.java
@@ -3,37 +3,31 @@ package J01013;
 import java.util.Scanner;
 
 public class Main {
+    // Using pre-calculate with position
+    // Still not optimize
+    public static final int MAX_SIZE = 2000001;
+    static long[] preCalRes = new long[MAX_SIZE];
+
     public static void main(String[] args) {
+        for (int i = 0; i < MAX_SIZE; i++) {
+            preCalRes[i] = 0;
+            int limit = (int) Math.sqrt(i);
+            for (int j = 2; j <= limit; j++) {
+                if (i % j == 0) {
+                    preCalRes[i] = preCalRes[j] + preCalRes[i / j];
+                    break;
+                }
+            }
+            if (preCalRes[i] == 0) preCalRes[i] = i;
+        }
+
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
         long total = 0;
         while (t-- > 0) {
-            long num = scanner.nextLong();
-            total += sumOfDivisors(num);
+            int num = scanner.nextInt();
+            total += preCalRes[num];
         }
         System.out.println(total);
-    }
-
-    static long sumOfDivisors(long num) {
-        long sum = 0;
-        long exponentOfTwo = 0;
-        while (num % 2 == 0) {
-            num /= 2;
-            exponentOfTwo++;
-        }
-        if (exponentOfTwo > 0) sum += 2 * exponentOfTwo;
-
-        for (long i = 3; i * i <= num; i += 2) {
-            long exponent = 0;
-            while (num % i == 0) {
-                num /= i;
-                exponent++;
-            }
-            if (exponent > 0) sum += exponent * i;
-        }
-        if (num > 1) {
-            sum += num;
-        }
-        return sum;
     }
 }
